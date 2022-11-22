e7d428a30f88c2ce04683778c8f0c0e580f020f6
OLoKo64 <naldoepingo1@gmail.com>
2022-11-21 22:34:48 -0300

    feat: add routes to project

---
+++ src/pages/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pa-8">
+    <div class="text-center">
+      <h2 class="mb-5">Upscale-rs</h2>
+      <p class="mb-8">A Tauri based GUI app that upscale images using Real-ESRGAN model.</p>
+      <div class="text-left">
+        <p><strong>Version: </strong>v{{ appVersion }}</p>
+        <p><strong>License: </strong>{{'MIT'}}</p>
+        <p><strong>Author: </strong>{{'OLoKo64 <reinaldorozatoj.11cg1@aleeas.com>'}}</p>
+        <p><strong>Repository: </strong>{{'https://github.com/OLoKo64/upscale-rs'}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api/tauri";
+import { onMounted, ref } from "vue";
+
+const appVersion = ref("");
+
+onMounted(async () => {
+  const result: string = await invoke("get_version");
+  appVersion.value = result;
+});
+</script>

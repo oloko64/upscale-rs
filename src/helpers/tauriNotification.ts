9002c70bcd1e1d69bcdf1d2d7b217ceeb7259dff
OLoKo64 <naldoepingo1@gmail.com>
2022-12-27 14:59:35 -0300

    feat: change alert to notification on success

---
+++ src/helpers/tauriNotification.ts
@@ -0,0 +1,21 @@
+import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
+
+
+/**
+ * Sends a notification to the user if the permission is granted, otherwise it requests the permission
+ * 
+ * @param title Title of the notification
+ * @param body Body of the notification
+ * @returns Promise<void>
+*/
+export async function sendTauriNotification(title: string, body: string) {
+    let permissionGranted = await isPermissionGranted();
+    if (!permissionGranted) {
+        const permission = await requestPermission();
+        permissionGranted = permission === 'granted';
+    }
+
+    if (permissionGranted) {
+        sendNotification({ title, body });
+    }
+}

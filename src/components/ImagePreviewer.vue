b4df945cada44adae8c77c0075addfdc1f50def3
OLoKo64 <naldoepingo1@gmail.com>
2022-12-27 19:19:06 -0300

    feat: add helper text on preview

--- src/components/ImagePreviewer.vue
+++ src/components/ImagePreviewer.vue
@@ -2,7 +2,11 @@
     <div>
         <viewer :images="images" />
         <div :class="{ 'preview-area': !isProcessing }" @click="showImagePreviewer">
-            <v-img :class="{ 'image-src-on-hover': !isProcessing }" class="image-src" :src="imageSrc" width="500" height="500" aspect-ratio="1" cover />
+            <v-img :class="{ 'image-src-on-hover': !isProcessing }" class="image-src" :src="imageSrc" width="500" height="500" aspect-ratio="1" cover >
+                <div class="preview-text-back px-3 py-2">
+                    <p class="ma-0">Click to preview</p>
+                </div>
+            </v-img>
         </div>
     </div>
 </template>
@@ -52,6 +56,18 @@ export default defineComponent({
     position: fixed;
 }
 
+.preview-text-back {
+    z-index: 1;
+    position: absolute;
+    top: 440px;
+    left: 180px;
+    color: #e7e7e7;
+    background-color: rgba(34, 34, 34, 0.7);
+    border-radius: 24px;
+    font-size: 20px;
+    font-weight: 500;
+}
+
 .image-src {
     border-radius: 24px;
     border: 2px solid rgba($color: #969696, $alpha: 0.4);
@@ -62,7 +78,7 @@ export default defineComponent({
 }
 
 .image-src-on-hover:hover {
-    filter: brightness(0.8);
+    filter: brightness(0.7);
     border: 2px solid rgba($color: #969696, $alpha: 0.8);
 }
 </style>

c10b9fd5d891df1051203246d881d0ec49eea632
OLoKo64 <naldoepingo1@gmail.com>
2022-12-27 13:07:11 -0300

    feat: add image previewer

---
+++ src/components/ImagePreviewer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <viewer :images="images" />
+        <div class="show-image-preview">
+            <v-btn elevation="0" @click="showImagePreviewer">Click to preview images</v-btn>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    props: {
+        images: {
+            type: Array,
+            default: () => []
+        }
+    },
+    methods: {
+        showImagePreviewer() {
+            this.$viewerApi({
+                images: this.images as string[],
+                options: {
+                    inline: true,
+                    button: false,
+                    toolbar: false,
+                    title: false,
+                    tooltip: false,
+                    rotatable: false,
+                    scalable: false,
+                    fullscreen: false,
+                    keyboard: false,
+                },
+            })
+        },
+    },
+})
+</script>
+<style scoped lang="scss">
+.show-image-preview {
+    z-index: 1;
+    top: 500px;
+    left: 500px;
+    position: fixed;
+}
+</style>

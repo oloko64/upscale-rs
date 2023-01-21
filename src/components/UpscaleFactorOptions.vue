2ba6919049f1345c94c5b15d543c5bed1848072f
OLoKo64 <naldoepingo1@gmail.com>
2023-01-21 02:26:22 -0300

    feat: lint using eslint

--- src/components/UpscaleFactorOptions.vue
+++ src/components/UpscaleFactorOptions.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="upscale-options">
-    <p style="margin-bottom: 2px">Select the upscale factor:</p>
-    <input type="radio" id="2" name="fav_language" value="2" @change="updateUpscaleFactor" />
-    <label style="margin-left: 5px" for="2">2x</label><br />
-    <input type="radio" id="3" name="fav_language" value="3" @change="updateUpscaleFactor" />
-    <label style="margin-left: 5px" for="3">3x</label><br />
-    <input checked type="radio" id="4" name="fav_language" value="4" @change="updateUpscaleFactor" />
-    <label style="margin-left: 5px" for="4">4x</label><br />
+    <p style="margin-bottom: 2px">
+      Select the upscale factor:
+    </p>
+    <input
+      id="2"
+      type="radio"
+      name="fav_language"
+      value="2"
+      @change="updateUpscaleFactor"
+    >
+    <label
+      style="margin-left: 5px"
+      for="2"
+    >2x</label><br>
+    <input
+      id="3"
+      type="radio"
+      name="fav_language"
+      value="3"
+      @change="updateUpscaleFactor"
+    >
+    <label
+      style="margin-left: 5px"
+      for="3"
+    >3x</label><br>
+    <input
+      id="4"
+      checked
+      type="radio"
+      name="fav_language"
+      value="4"
+      @change="updateUpscaleFactor"
+    >
+    <label
+      style="margin-left: 5px"
+      for="4"
+    >4x</label><br>
   </div>
 </template>
 
